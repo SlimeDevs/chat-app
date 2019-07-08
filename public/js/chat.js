4c59fe48c9a201abf763f9857cbc47533a02931a
SlimeDevs <46899559+SlimeDevs@users.noreply.github.com>
2019-07-07 20:06:30 -0500

    Added location/message acknowledgements and profanity filter

--- public/js/chat.js
+++ public/js/chat.js
@@ -9,7 +9,14 @@ socket.on('message', (messageValue) => {
 document.querySelector('#message-form').addEventListener('submit', (event) => {
 	event.preventDefault() // Stops browser reload
 	const message = event.target.elements.message.value;
-	socket.emit('sendMessage', message)
+
+	socket.emit('sendMessage', message, (error) => {
+		if (error) {
+			return console.log(error)
+		}
+
+		console.log('Message delivered!')
+	})
 })
 
 document.querySelector('#send-location').addEventListener('click', () => {
@@ -23,6 +30,8 @@ document.querySelector('#send-location').addEventListener('click', () => {
 		socket.emit('sendLocation', { 
 			latitude: position.coords.latitude,
 			longitude: position.coords.longitude
+		}, () => {
+			console.log('Location Shared!')
 		})
 	})
 })
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import http from "http";
 import socketio from "socket.io";
 import GeoLocation from "./interfaces/GeoLocation";
 import { port } from "./util/env";
+import Filter from "bad-words";
 const app = express();
 const server = http.createServer(app)
 const io = socketio(server)
@@ -16,13 +17,19 @@ io.on('connection', function(socket) {
 	socket.emit('message', 'Welcome!')
 	socket.broadcast.emit('message', 'A new user has joined')
 
-	socket.on('sendMessage', (message: string) => {
-		console.log(message)
+	socket.on('sendMessage', (message: string, callback: Function) => {
+		// Returns error if message contains profanity
+		const filter = new Filter()
+		if (filter.isProfane(message)) return callback('Profanity is not allowed!')
+		
+		// Otherwise, sends message to all clients
 		io.emit('message', message)
+		callback('Message Delivered!')
 	})
 
-	socket.on('sendLocation', (location: GeoLocation) => {
-		io.emit('message', `Location: ${location.latitude} latitude, ${location.longitude} longitude`)
+	socket.on('sendLocation', (location: GeoLocation, callback: Function) => {
+		io.emit('message', `https://google.com/maps?q=${location.latitude},${location.longitude}`)
+		callback('Location Shared')
 	})
 
 	socket.on('disconnect', () => {
